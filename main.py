42123fa56c28ddb7e569d2dd0d531cbcc83c2d55
Gleb Plotnikov <g_plotnikov@mail.ru>
2026-02-13 00:11:24 +0300

    fixed main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from api.rest_api import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,thon 
+        app, 
         host="0.0.0.0",
-        port=8085,
+        port=8080,
         log_level="info"
     )
